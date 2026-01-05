0b3396c4b4a0c466460e88827d89ba50e1cf2885
Tsptds <152429392+Tsptds@users.noreply.github.com>
2026-01-05 23:03:17 +0300

    Increased jump height for standing jump

--- src/Hooks.hpp
+++ src/Hooks.hpp
@@ -70,9 +70,14 @@ namespace Hooks {
                 }
 
                 else if (ev == "jumpBegin") {
-                    auto &JH = actor->GetCharController()->jumpHeight;
+                    const auto &ctrl = actor->GetCharController();
+                    auto &JH = ctrl->jumpHeight;
                     // logger::info("{}", JH);
-                    JH = 1.2f;  // Default 1.08585
+                    // Luckily event fires after jump height is set, so I can overwrite it here, it's set for every jump individually
+                    bool isStandingjump;
+                    actor->GetGraphVariableBool("_HORSE_IncreasedJump", isStandingjump);
+
+                    JH = isStandingjump ? 2.5f : 1.2f;  // Default 1.08585
                 }
 
                 return _ProcessEvent(a_this, a_event, a_eventSource);
