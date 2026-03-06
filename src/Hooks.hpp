538b7f7dcca33bb9c0160b5d49f8c8b1c0bfe997
Tsptds <152429392+Tsptds@users.noreply.github.com>
2026-03-06 03:23:28 +0300

    Fix stuck weapon & post mount walking horse in water

--- src/Hooks.hpp
+++ src/Hooks.hpp
@@ -26,8 +26,22 @@ namespace Hooks {
             static inline RE::BSEventNotifyControl Hook(RE::BSAnimationGraphManager *a_this, const RE::BSAnimationGraphEvent *a_event,
                                                         RE::BSTEventSource<RE::BSAnimationGraphEvent> *a_eventSource) {
                 const auto &actor = a_this->graphs[a_this->GetRuntimeData().activeGraph]->holder;
+                if (!actor) return _ProcessEvent(a_this, a_event, a_eventSource);
 
-                if (!actor || !actor->IsHorse()) return _ProcessEvent(a_this, a_event, a_eventSource);
+                if (actor->IsPlayerRef()) {
+                    /* Stuck swimming bool fix, can't draw weapons bug */
+                    if (a_event->tag == "MountEnd") {
+                        actor->AsActorState()->actorState1.swimming = false;
+
+                        /* Horse walking in water fix */
+                        RE::ActorPtr horse;
+                        if (actor->GetMount(horse) && horse) {
+                            if (horse->AsActorState()->actorState1.swimming) horse->NotifyAnimationGraph("MountedSwimStart");
+                        }
+                    }
+                    return _ProcessEvent(a_this, a_event, a_eventSource);
+                }
+                if (!actor->IsHorse()) return _ProcessEvent(a_this, a_event, a_eventSource);
                 const auto &ev = a_event->tag;
 
                 /* Ragdoll enable activation */
